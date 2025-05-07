d24cfe93b13ba5855675f89a99fa84a0c537e867
mohamed dawood <mohameddawood17919@gmail.com>
2025-05-07 10:35:36 +0300

    finish register screen ui

--- lib/core/constants/text_styles.dart
+++ lib/core/constants/text_styles.dart
@@ -1,35 +1,36 @@
+import 'package:alefk/core/themes/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class TextStyles {
   // Regular Text Styles
-  static const TextStyle small =
-      TextStyle(fontSize: 12.0, fontWeight: FontWeight.normal);
-  static const TextStyle medium =
-      TextStyle(fontSize: 16.0, fontWeight: FontWeight.normal);
-  static const TextStyle large =
-      TextStyle(fontSize: 20.0, fontWeight: FontWeight.normal);
-  static const TextStyle extraLarge =
-      TextStyle(fontSize: 24.0, fontWeight: FontWeight.normal);
+  static  TextStyle small =
+      TextStyle(fontSize: 12.0, fontWeight: FontWeight.normal, color: AppColors.current.text);
+  static  TextStyle medium =
+      TextStyle(fontSize: 16.0, fontWeight: FontWeight.normal, color: AppColors.current.text);
+  static  TextStyle large =
+      TextStyle(fontSize: 20.0, fontWeight: FontWeight.normal, color: AppColors.current.text);
+  static  TextStyle extraLarge =
+      TextStyle(fontSize: 24.0, fontWeight: FontWeight.normal, color: AppColors.current.text);
 
   // Bold Text Styles
-  static const TextStyle smallBold =
-      TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold);
-  static const TextStyle mediumBold =
-      TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold);
-  static const TextStyle largeBold =
-      TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
-  static const TextStyle extraLargeBold =
-      TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold);
+  static  TextStyle smallBold =
+      TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold, color: AppColors.current.text);
+  static  TextStyle mediumBold =
+      TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, color: AppColors.current.text);
+  static  TextStyle largeBold =
+      TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: AppColors.current.text);
+  static  TextStyle extraLargeBold =
+      TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: AppColors.current.text);
 
   // Italic Text Styles
-  static const TextStyle smallItalic =
-      TextStyle(fontSize: 12.0, fontStyle: FontStyle.italic);
-  static const TextStyle mediumItalic =
-      TextStyle(fontSize: 16.0, fontStyle: FontStyle.italic);
-  static const TextStyle largeItalic =
-      TextStyle(fontSize: 20.0, fontStyle: FontStyle.italic);
-  static const TextStyle extraLargeItalic =
-      TextStyle(fontSize: 24.0, fontStyle: FontStyle.italic);
+  static  TextStyle smallItalic =
+      TextStyle(fontSize: 12.0, fontStyle: FontStyle.italic, color: AppColors.current.text);
+  static  TextStyle mediumItalic =
+      TextStyle(fontSize: 16.0, fontStyle: FontStyle.italic, color: AppColors.current.text);
+  static  TextStyle largeItalic =
+      TextStyle(fontSize: 20.0, fontStyle: FontStyle.italic, color: AppColors.current.text);
+  static  TextStyle extraLargeItalic =
+      TextStyle(fontSize: 24.0, fontStyle: FontStyle.italic, color: AppColors.current.text);
 
   // Customizable Text Styles
   static TextStyle custom({
