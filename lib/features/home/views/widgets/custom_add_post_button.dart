497bc9c22ec35550e57bee68756ee4fff91ca817
MohamedAatef12 <mohamedateefmmeekk@gmail.com>
2025-05-08 10:26:39 +0300

    finish edit profile screen ui

--- lib/features/home/views/widgets/custom_add_post_button.dart
+++ lib/features/home/views/widgets/custom_add_post_button.dart
@@ -3,6 +3,7 @@ import 'package:alefk/core/constants/radius.dart';
 import 'package:alefk/core/constants/sized_box.dart';
 import 'package:alefk/core/themes/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAddPostButton extends StatelessWidget {
   const CustomAddPostButton({super.key});
@@ -12,7 +13,9 @@ class CustomAddPostButton extends StatelessWidget {
     return InkWell(
       borderRadius: RadiusConstants.large,
       splashColor: AppColors.current.blue.withOpacity(0.2),
-      onTap: () {},
+      onTap: () {
+        context.push('/add-post');
+      },
       child: Container(
         width: MediaQuery.of(context).size.width * 0.9,
         height: MediaQuery.of(context).size.height * 0.07,
